6ede67a49e9f2608f573e8d2d5cb11c0bc553b8b
Chris <foxc121@yahoo.co.uk>
2022-04-06 11:28:42 +0100

    first commit

---
+++ pages/about.js
@@ -0,0 +1,11 @@
+
+
+export default function about() {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>This is an app that helps to find the latest local DJ and other live musical events </p>
+      <p>Version 1.0.0</p>
+    </div>
+  )
+}
---
+++ pages/events/[slug].js
@@ -0,0 +1,9 @@
+
+
+export default function EventsPage() {
+  return (
+    <div>
+        <h1>My Event</h1>
+    </div>
+  )
+}
